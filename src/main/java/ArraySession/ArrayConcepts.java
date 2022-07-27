76e322a4ade337e49c4bbb440cb7fe7da3b631e9
Lalitha1256 <109729339+Lalitha1256@users.noreply.github.com>
2022-07-27 15:27:25 +0530

    AddingMethods

--- src/main/java/ArraySession/ArrayConcepts.java
+++ src/main/java/ArraySession/ArrayConcepts.java
@@ -1,5 +1,27 @@
 package ArraySession;
 
 public class ArrayConcepts {
-    int a[]={10,20,30,40,50,60};
+    static int[] a ={10,20,30,40,50,60};
+    static int[] b=new int[6];
+
+    //String name[]={"lalitha"};
+    //boolean c[]={true,false};
+
+    public static void main(String[] args) {
+        System.out.println(a.length);
+        b[0]=10;
+        b[1]=20;
+        b[2]=30;
+        b[3]=40;
+        b[4]=50;
+        b[5]=60;
+        System.out.println(b[0]);
+        System.out.println(b[1]);
+        System.out.println(b[2]);
+        System.out.println(b[3]);
+        System.out.println(b[4]);
+        System.out.println(b[5]);
+    }
 }
+
+
