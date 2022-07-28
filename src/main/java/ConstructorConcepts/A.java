d552166d56f4b35fb65b563bfb2f3410df951215
Lalitha1256 <109729339+Lalitha1256@users.noreply.github.com>
2022-07-28 21:31:37 +0530

    AddingMethods

---
+++ src/main/java/ConstructorConcepts/A.java
@@ -0,0 +1,14 @@
+package ConstructorConcepts;
+
+import java.sql.SQLOutput;
+
+public class A {
+    public A(int a){
+        System.out.println(a);
+    }
+    public void add(int a,int b){
+        int c=a+b;
+        System.out.println(c);
+    }
+
+}
--- src/main/java/Stringsessions/Stringexample.java
+++ src/main/java/Stringsessions/Stringexample.java
@@ -3,13 +3,13 @@ package Stringsessions;
 public class Stringexample {
     static String s1 = "lalitha";
     static String s2 = "sri";
-    static String s3=new String("i love srinivas");
-    static String firstname="ambica";
-    static String lastname="rani";
+    static String s3 = new String("i love srinivas");
+    static String firstname = "ambica";
+    static String lastname = "rani";
     static String fullname;
 
     public static void main(String[] args) {
-        System.out.println(s1.length());
+        /*System.out.println(s1.length());
         System.out.println(s2.length());
         System.out.println(s1.toLowerCase());
         System.out.println(s1.toUpperCase());
@@ -22,6 +22,17 @@ public class Stringexample {
         fullname=firstname.concat(lastname);
         System.out.println(fullname);
         System.out.println("fullname of the String is :"+fullname );
+*/
+        String s1 = "naveen";
+        String s2 = "naveen";
+        System.out.println(s1==s2);
+        String s3 = new String("my name");
+        String s4 = new String("my name is");
+        System.out.println(s3.equalsIgnoreCase(s4));
+        System.out.println(s1.indexOf("naveen"));
+        System.out.println(s1.substring(11));
+
     }
 }
+
 
